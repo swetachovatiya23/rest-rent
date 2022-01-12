dcd642a508d67b6f143d3ddbb92ea371fc2671b7
swetachovatiya23 <swetachovatiya2312@gmail.com>
2022-01-11 19:57:41 -0500

    basic setup

---
+++ index.js
@@ -0,0 +1,17 @@
+// Require needed modules.
+const express = require('express')
+
+// Initialize the app object.
+const app = express()
+
+// Create a homepage route.
+app.get('/', function (req, res) {
+    // This gets sent to the client
+    // (your web browser most likely!)
+    res.send('Hello world')
+})
+
+// Listen for connections.
+app.listen(3000, function () {
+    console.log('I am awake!')
+})

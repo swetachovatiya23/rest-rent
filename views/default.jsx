c6594a158b3778d236958b75616771283dcdd6f8
swetachovatiya23 <swetachovatiya2312@gmail.com>
2022-01-17 12:02:12 -0500

    Adding style to REST-RENT

--- views/default.jsx
+++ views/default.jsx
@@ -5,12 +5,14 @@ function Def (html) {
         <html>
             <head>
                 <title>Title</title>
+                {/* BOOTSTRAP CDN LINK (long)  */}
+                <link rel="stylesheet" href="/css/style.css"/>
             </head>
             <body>
                 {html.children}
             </body>
         </html>
     )
-  }
+}
 
 module.exports = Def

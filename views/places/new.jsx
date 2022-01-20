b39f60f78be2940613d52e5b29e0b0e48ef1416f
swetachovatiya23 <swetachovatiya2312@gmail.com>
2022-01-20 13:18:58 -0500

    added founded year

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -6,34 +6,51 @@ function new_form () {
         <Def>
           <main>
             <h1>Add a New Place</h1>
-            <form method="POST" action="/places">
-                <div className="form-group">
-                  <label htmlFor="name">Place Name</label>
-                  <input className="form-control" id="name" name="name" required />
+            <div className="col-sm-6 col-md-4 col-lg-3">
+                <label htmlFor="name">Place Name</label>
+                 <input className="form-control" id="name" name="name" required />
+            </div>
+            
+
+            
+            <div className="col-sm-6 col-md-4 col-lg-3">
+                <label htmlFor="cuisine">Cuisine</label>
+                 <input className="form-control" id="cuisines" name="cuisines" required />
+            </div>
+        
+
+            <div className="col-sm-6 col-md-4 col-lg-3">
+                <label htmlFor="pic">Place Picture</label>
+                 <input className="form-control" id="pic" name="pic" />
                 </div>
-                <div className="form-group">
-                  <label htmlFor="pic">Place Picture</label>
-                  <input className="form-control" id="pic" name="pic" />
+            
+
+            <div className="col-sm-6 col-md-4 col-lg-3">
+                <label htmlFor="city">City Name</label>
+                 <input className="form-control" id="city" name="city" />
                 </div>
-                <div className="form-group">
-                  <label htmlFor="city">City</label>
-                  <input className="form-control" id="city" name="city" />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="state">State</label>
-                  <input className="form-control" id="state" name="state" />
+            
+
+            <div className="col-sm-6 col-md-4 col-lg-3">
+                <label htmlFor="state">State</label>
+                 <input list="state-list" id="state" name="state"/>
                 </div>
                 <div className="form-group">
                   <label htmlFor="cuisines">Cuisines</label>
                   <input className="form-control" id="cuisines" name="cuisines" required />
                 </div>
                 <input className="btn btn-primary" type="submit" value="Add Place" />
-              </form>
+              
 
-              <div className="form-group">
-                <label for="founded">Founded Year</label>
-                <input className="form-control" id="founded" name="founded" />
-              </div>
+              <div className="form-group col-sm-4">
+              <label htmlFor="founded">Founded Year</label>
+                     <input 
+                      type="number" 
+                     className="form-control" 
+                     id="founded" 
+                     name="founded" 
+                    value={new Date().getFullYear()} />
+            </div>
           </main>
         </Def>
     )
